1107540a90784bbc96f2dc48495c50e65650e9a7
andrelefranco001 <andrelefranco001@gmail.com>
2025-02-23 19:01:38 -0500

    fixed description js

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -3,8 +3,11 @@ const projectsData = [
     id: "barryandbrothers",
     title: "Barry & Brothers: A Legacy Woven in Tradition",
     description: `Over a century ago, in a small tailor shop in London, two brothers passionate about craftsmanship and quality set out to create more than just clothing—they wanted to design pieces that told stories. Thus, Barry & Brothers was born, a brand that, with every intertwined thread of cashmere, built a legacy of elegance, tradition, and exclusivity.
+
     From the very beginning, the brand became synonymous with understated luxury. It was never just about sweaters and coats but about pieces that accompanied entire generations, carrying the warmth of expert craftsmanship and the sophistication of timeless design. Every stitch bore the dedication of master artisans, and every fabric reflected an unwavering commitment to excellence.
+
     As time passed, Barry & Brothers evolved without losing its essence. Rooted in tradition yet firmly grounded in the present, the brand crafted an experience tailored for those who appreciate simplicity without sacrificing refinement. Every detail, from the selection of materials to the seamless digital experience, was designed to convey the exclusivity and durability that define its identity.
+
     Today, choosing Barry & Brothers means acquiring more than just a garment—it means embracing a piece of history. A coat that will stand the test of time with the same dignity with which it was created, a sweater that will wrap moments and memories. Because within every fiber beats the spirit of a brand that understands true elegance is the kind that endures.`,
     bannerImages: [
       "../../src/assets/pdbanner02.png",
